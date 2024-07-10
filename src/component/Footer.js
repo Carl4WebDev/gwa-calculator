21fecbb04021cb58108b24f18fd1e5c9a2733a90
carl Ivan Sampan <carlforwebdev@gmail.com>
2024-07-10 21:32:37 +0600

    footer in master

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -13,17 +13,6 @@ export const Footer = () => {
                     </span>
                 </span>
             </a>
-            <a target="_blank" href="https://carlivansampan-sample-portfolio.netlify.app"> 
-                <span className="flex items-center justify-start mr-15 bg-white p-4 rounded-full">
-                    <svg  xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                    </svg>
-                    <span className="mx-5 text-amber-400 text-3xl font-black tracking-tighter">
-                        Sample Portfolio
-                    </span>
-                </span>
-            </a>
     
         </div>
     </footer>
